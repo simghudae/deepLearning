0e418e1696b69e83cf03db931169843deb7151f8
ghudaesim <gudae7@gmail.com>
2020-03-09 11:46:43 +0900

    add DFS case

---
+++ subProject.py
@@ -0,0 +1,28 @@
+computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+n = 3
+
+def dfs(graph, start_node, answerList):
+    visit, stack = [],[]
+    stack.append(start_node)
+
+    while stack:
+        node = stack.pop()
+        if node not in visit:
+            visit.append(node)
+            answerList[node] = 1
+            _list = [i for i, x in enumerate(graph[node]) if x == 1]
+            _list.remove(node)
+            stack.extend(_list)
+
+def solution(n, computers):
+    answerList = [0 for _ in range(n)]
+    answer = 0
+    for i in range(len(computers)):
+        if answerList[i] == 0:
+            answer += 1
+            dfs(computers, i, answerList)
+
+        else:
+            continue
+
+    return answer
