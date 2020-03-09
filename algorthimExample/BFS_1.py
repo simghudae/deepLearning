8b7b44adebdda33b6a8f4d8dcef319af80d7cda8
ghudaesim <gudae7@gmail.com>
2020-03-09 17:41:37 +0900

    add BFS Case

--- algorthimExample/BFS_1.py
+++ algorthimExample/BFS_1.py
@@ -1,5 +1,4 @@
 def BFS(graph, startNode):
-    #graph is dict type
     visit, queue = [], []
     queue.append(startNode)
     while queue:
@@ -9,3 +8,39 @@ def BFS(graph, startNode):
             queue.append(graph[node])
 
     return visit
+
+
+def diff(node, word):
+    _list = [1 if _a != _b else 0 for _a, _b in zip(node, word)]
+    return sum(_list)
+
+
+def solution(begin, target, words):
+    answer = 0
+    visit, queue, _queue = [], [], []
+    queue.append(begin)
+
+    while queue:
+        node = queue.pop(0)
+        if node == target:
+            return answer
+
+        elif node not in visit:
+            visit.append(node)
+            _list = [_w for _w in words if diff(node, _w) == 1]
+            _queue.extend(_list)
+
+        if not queue:
+            queue.extend(_queue)
+            _queue = []
+            answer += 1
+
+    else:
+        return 0
+
+
+begin = 'hit'
+target = 'cog'
+words = ['hot', 'dot', 'dog', 'lot', 'log', 'cog']
+
+solution(begin, target, words)
