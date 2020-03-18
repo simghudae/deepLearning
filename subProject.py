f1f633537c6ddbc363e90ea9ee8b7d85c20cb006
ghudaesim <gudae7@gmail.com>
2020-03-18 16:41:53 +0900

    add Tensorflow

--- subProject.py
+++ subProject.py
@@ -28,24 +28,69 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer)
 
 # ---------------------------
-
+import os
 import tensorflow as tf
 import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 x_data = np.array([[0, 0], [1, 0], [1, 1], [0, 0], [0, 0], [0, 1]])
 y_data = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 0, 0], [1, 0, 0], [0, 0, 1]])
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
+# counting global step
+global_step = tf.Variable(0, trainable=False, name='global_step')
 
-W = tf.Variable(tf.random_uniform([2, 3], -1., 1.))
-b = tf.Variable(tf.zeros([3]))
+with tf.name_scope('layer1'):
+    W1 = tf.Variable(tf.random_uniform([2, 3], -1., 1.), name='W1')
+    b1 = tf.Variable(tf.zeros([3]), name='b1')
+    L1 = tf.add(tf.matmul(X, W1), b1)
+    L1 = tf.nn.relu(L1)
 
-L = tf.add(tf.matmul(X, W), b)
-L = tf.nn.relu(L)  # activate ftn
-L = tf.nn.softmax(L)  # sum =1 normalize
+with tf.name_scope('layer2'):
+    W2 = tf.Variable(tf.random_uniform([3, 3], -1., 1.), name='W2')
+    b2 = tf.Variable(tf.zeros([3]), name='b2')
+    L2 = tf.add(tf.matmul(L1, W2), b2)
 
-cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(L), axis=1))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-train_op = optimizer.minimize(cost)
+with tf.name_scope('optimizer'):
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y, logits=L2))
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+    train_op = optimizer.minimize(cost, global_step=global_step)
+
+    # tensor board
+    tf.summary.scalar('cost', cost)  # 값이 한개인 tensor를 수집하는 방법
+
+
+# create session
+sess = tf.Session()
+saver = tf.train.Saver(tf.global_variables())
+
+# restore tensor
+ckpt = tf.train.get_checkpoint_state('./model')
+if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+    saver.restore(sess, ckpt.model_checkpoint_path)
+else:
+    sess.run(tf.global_variables_initializer())
+
+#tensor board1
+merged = tf.summary.merge_all()
+writer = tf.summary.FileWriter('./logs', sess.graph)
+
+for step in range(100):
+    sess.run(train_op, feed_dict={X: x_data, Y: y_data})
+    if (step + 1) % 10 == 0:
+        print(step + 1, sess.run(cost, feed_dict={X: x_data, Y: y_data}))
+
+    #tensor board2
+    summary = sess.run(merged, feed_dict={X: x_data, Y: y_data})
+    writer.add_summary(summary, global_step=sess.run(global_step))
+
+
+
+# save tensor
+saver.save(sess, './model/dnn.ckpt', global_step=global_step)
+sess.close()
+
 
