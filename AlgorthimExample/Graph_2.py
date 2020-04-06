266549d712c2d3174ce8d4e06fa92ce09bff8e1d
ghudaesim <gudae7@gmail.com>
2020-04-06 18:13:06 +0900

    add graph2

--- AlgorthimExample/Graph_2.py
+++ AlgorthimExample/Graph_2.py
@@ -1,7 +1,6 @@
 n = 6
 edge = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
-
-
+#https://programmers.co.kr/learn/courses/30/lessons/49189
 def solution(n, edge):
     edgeDict = {i: [] for i in range(1, n + 1)}
     for [i, j] in edge:
