84ca0035138c83d7935f481a6dd08b2c38ddaade
ghudaesim <gudae7@gmail.com>
2020-05-01 21:04:46 +0900

    add folder

--- subProject.py
+++ subProject.py
@@ -1,43 +1,16 @@
-number = '10000'
-k = 2
-
-#https://programmers.co.kr/learn/courses/30/lessons/42883
-
-def smallCase(n, j, answer, count, k):
-    if len(answer) == 0:
-        answer.append(n)
-        return answer, count, j, False
-    elif count >= k:
-        answer.append(n)
-        return answer, count, j, False
-    elif answer[j] > n:
-        answer.append(n)
-        return answer, count, j, False
-    elif answer[j] == n:
-        answer.append(n)
-        return answer, count, j, False
-    elif answer[j] < n:
-        answer.pop(j)
-        return answer, count + 1, j - 1, True
+n = 4
+costs = [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]
 
 
-def solution(number, k):
-    ans, count = [int(number[0])], 0
-    for i in range(1, len(number)):
-        # print(ans, number[i], count)
-        j, Flags = len(ans) - 1, True
-        while Flags:
-            ans, count, j, Flags = smallCase(int(number[i]), j, ans, count, k)
+def solution(n, costs):
+    costDict = {i: [] for i in range(n)}
+    for i in costs:
+        costDict[i[0]].append([i[1], i[2]])
+        costDict[i[1]].append([i[0], i[2]])
 
-    answer = ''
-    if count == k:
-        for c in ans:
-            answer += str(c)
-    else:
-        for i, c in enumerate(ans):
-            if i < len(ans)-k:
-                answer += str(c)
-            else:
-                break
 
+    while True:
+
+
+    answer = 0
     return answer
