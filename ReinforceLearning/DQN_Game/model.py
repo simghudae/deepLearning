eb5d577581afefd00202f1ce7372827116e8bc08
gudae <gudae7@gmail.com>
2020-05-04 08:00:37 +0900

    add DQN model

---
+++ ReinforceLearning/DQN_Game/model.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+
+class DQN:
+
+    def __init__(self, screenHeight, screenWidth, nAction):
+        self.screenHeight, self.screenWidth = screenHeight, screenWidth
+        self.nAction = nAction
+
+    def _buildNetwork(self):
+
+    def _buildOp(self):
+
+    def updateTargetNetwork(self):
+
+    def getAction(self):
+
+    def train(self):
+
+    def initState(self):
+
+    def remember(self):
+
+    def _sampleMemory(self):
